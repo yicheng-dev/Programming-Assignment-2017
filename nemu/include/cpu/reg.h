7c5e7a03c8b8b05958daf5aac530addc5ca89281
tracer-ics2017 <tracer@njuics.org>
2017-09-10 08:45:17 -0700

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  08:45:17 up 44 min,  1 user,  load average: 0.25, 0.18, 0.11 787119edeed231f4bb5236ba627f66777f2c998d

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,13 +15,15 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef union{
+	union{
 
 	union{
     uint32_t _32;
     uint16_t _16;
     uint8_t _8[2];
   } gpr[8];
-
+	vaddr_t eip;
+	};
   /* Do NOT change the order of the GPRs' definitions. */
 
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
@@ -29,9 +31,6 @@ typedef union{
    */
 	struct{
   rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-	};
-	struct{
-  vaddr_t eip;
 	};
 } CPU_state;
 
