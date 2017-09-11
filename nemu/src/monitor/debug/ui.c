8b03036a0d3b58e8c4c25ed21b54bea80966372c
tracer-ics2017 <tracer@njuics.org>
2017-09-10 22:12:52 -0700

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  22:12:52 up 50 min,  1 user,  load average: 0.06, 0.03, 0.00 549b3ff5978af4838c017fe7cd56f76fb7bd4e1

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -71,7 +71,7 @@ static int cmd_x(char *args)
 {
 	int N=atoi(args);
 	args=strtok(NULL," ");
-	int args_int=atoi(args);
+	uint64_t args_int=atoi(args);
 	for (int i=0;i<N;i++)
 	{
 		printf("%u\n",pmem[args_int+4*i]);
