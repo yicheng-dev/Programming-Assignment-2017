5e10874b6ee18e4db1f541518d0747e8e6463ef4
tracer-ics2017 <tracer@njuics.org>
2017-09-15 22:20:42 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  22:20:42 up 18 min,  1 user,  load average: 0.02, 0.05, 0.07 96c0919b322733827e76b43e80cca7698decd10a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -134,6 +134,7 @@ uint32_t expr(char *e, bool *success) {
   }
   for (int i=0;i<nr_token;i++)
   {
+	  printf("%d\n",tokens[i].type);
 	  if (tokens[i].type==248)
 	  {
 		  printf("%s\n",tokens[i].str);
