e8d3dcf7eea9a02d9c8e589b2d8467f6d10799b9
tracer-ics2017 <tracer@njuics.org>
2017-09-15 22:07:58 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  22:07:58 up 6 min,  1 user,  load average: 0.04, 0.18, 0.10 97d3ba6813bec5a97c2bbc0eb7f349cdf58c1a91

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -132,9 +132,9 @@ uint32_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-
+  
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+//  TODO();
 
   return 0;
 }
