ea599c6c6dac19a0e4667edd387e5ac84697c500
tracer-ics2017 <tracer@njuics.org>
2017-09-18 16:28:40 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:28:40 up 24 min,  1 user,  load average: 0.01, 0.08, 0.12 a3e72bdf26093a039350b8386e308a93d9e0019

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -130,7 +130,7 @@ static int cmd_p(char *args)
 	uint32_t ans;
 	ans = expr(args,success_ptr);
 	if (*success_ptr)
-		printf("UnsignedDec: %u\tSignedDrc: %d\tHex: %x\n",ans,ans,ans);
+		printf("UnsignedDec: %u\tSignedDrc: %d\tHex: 0x%x\n",ans,ans,ans);
 	else
 		printf("Something was wrong with your command!\n");
 	return 0;
