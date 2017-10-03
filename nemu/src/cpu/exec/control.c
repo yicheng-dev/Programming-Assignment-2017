cb9b2c81fd0843769b80af4c5e04e16533a28f19
tracer-ics2017 <tracer@njuics.org>
2017-10-03 17:59:40 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  17:59:40 up  7:20,  1 user,  load average: 0.44, 0.22, 0.09 fa026912793e05bc9639e3a631f93ccae5bd2728

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -26,7 +26,7 @@ make_EHelper(jmp_rm) {
 make_EHelper(call) {
   // the target address is calculated at the decode stage
 //  TODO();
-
+  decoding.jmp_eip = id_dest->val;
   decoding.is_jmp = 1;
   print_asm("call %x", decoding.jmp_eip);
 }
