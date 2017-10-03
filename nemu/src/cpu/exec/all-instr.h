88d207cc40f16fadac18831936e23bffaf3d6fbf
161220049-Yicheng HUANG <453541724@qq.com>
2017-10-03 15:42:59 +0800

    modified:   all-instr.h 	modified:   exec.c

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,7 +1,12 @@
 #include "cpu/exec.h"
 
 make_EHelper(mov);
-
+make_EHelper(call);
+make_EHelper(push);
+make_EHelper(pop);
+make_EHelper(xor);
+make_EHelper(ret);
+make_EHelper(sub);
 make_EHelper(operand_size);
 
 make_EHelper(inv);
