f920ccf6c4aeb950a8bc9b2b82bd43dcc0eb5ac8
tracer-ics2017 <tracer@njuics.org>
2017-10-03 16:41:32 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:41:32 up  6:02,  1 user,  load average: 0.09, 0.12, 0.09 4cca3bc1654a17ca1dcce912194c4a8f5777bd40

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -270,7 +270,7 @@ make_DHelper(J) {
 
 make_DHelper(call_SI){
   decode_op_SI(eip, id_dest, false);
-  decoding.jmp_eip = id_dest->simm + *eip;
+  decoding.jmp_eip = id_src->simm;
 }
 
 make_DHelper(push_SI) {
