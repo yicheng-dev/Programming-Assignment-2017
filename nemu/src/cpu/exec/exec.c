6453bfa9d7b15a8257b7b3ec39bd376406d887e1
161220049-Yicheng HUANG <453541724@qq.com>
2017-10-05 13:37:55 +0800

    modified:   exec.c

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -62,7 +62,7 @@ make_group(gp4,
 
   /* 0xff */
 make_group(gp5,
-    EMPTY, EMPTY, EX(call_rm), EMPTY,
+    EMPTY, EMPTY, EX(call_rm), EX(call),
     EX(jmp_rm), EX(jmp), EMPTY, EMPTY)     
 
   /* 0x0f 0x01*/
