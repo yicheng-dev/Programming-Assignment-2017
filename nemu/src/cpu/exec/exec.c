67dbce1b726d3ca67ded835b6afda787a52ac180
tracer-ics2017 <tracer@njuics.org>
2017-10-05 12:38:31 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:38:31 up 21:25,  1 user,  load average: 0.41, 0.13, 0.10 81dd0501bcd8287f666c05fc15578765781960f

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -63,7 +63,7 @@ make_group(gp4,
   /* 0xff */
 make_group(gp5,
     EMPTY, EMPTY, EMPTY, EMPTY,
-    EX(jmp), EX(jmp), EMPTY, EMPTY)
+    EX(jmp), EX(jmp), EMPTY, EMPTY)     /*关于两个EX(jmp)还需验证*/
 
   /* 0x0f 0x01*/
 make_group(gp7,
