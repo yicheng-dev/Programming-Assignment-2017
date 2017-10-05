c3cdf80a90b8f78c2c3c98b76185bf4c9c14cad4
tracer-ics2017 <tracer@njuics.org>
2017-10-05 23:40:50 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  23:40:50 up 1 day,  7:15,  1 user,  load average: 0.41, 0.38, 0.26 f5c2a8f4ce394d158e8fe178718edee83ea486f0

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -268,11 +268,9 @@ make_DHelper(a2O) {
 }
 
 make_DHelper(J_rel) {
-  decode_op_SI(eip, id_dest, true);
+  decode_op_SI(eip, id_dest, false);
   // the target address can be computed in the decode stage
-  // decoding.jmp_eip = id_dest->simm + *eip;
-  rtl_add(&id_dest->val,eip,&id_dest->val);
-  decoding.jmp_eip = id_dest->val;
+  decoding.jmp_eip = id_dest->simm + *eip;
 }
 
 make_DHelper(J_ptr){
