09c67e21b2a6198cae3e3bc92c8d6d159579bedc
tracer-ics2017 <tracer@njuics.org>
2017-10-06 16:47:46 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:47:46 up 1 day, 14:58,  1 user,  load average: 0.41, 0.29, 0.20 d39623c19c96bd6b9924ba782f363efe380779fb

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -189,6 +189,7 @@ static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   *dest = (*src1)>>(width * 8 - 1);
+  printf("%d\n",*dest);
 	// dest <- src1[width * 8 - 1]
 }
 
