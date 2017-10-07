f826fa899e7e26f646c2ad51f31cdf7a4b983096
tracer-ics2017 <tracer@njuics.org>
2017-10-07 16:28:34 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:28:34 up  6:07,  1 user,  load average: 0.74, 0.27, 0.14 d19c53dac8ab18fe1418a74e6b6ba275cbcf79b9

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -187,7 +187,7 @@ opcode_entry opcode_table [512] = {
   /* 0xb0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xb4 */	EMPTY, EMPTY, IDEX(mov_E2G,movzx), IDEX(mov_E2G,movzx),
   /* 0xb8 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0xbc */	EMPTY, EMPTY, IDEX(mov_E2G,movsx), IDEX(mov_E2G,movsx),
+  /* 0xbc */	EMPTY, EMPTY, IDEX(mov_Eb2G,movsx), IDEX(mov_E2G,movsx),
   /* 0xc0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc4 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xc8 */	EMPTY, EMPTY, EMPTY, EMPTY,
