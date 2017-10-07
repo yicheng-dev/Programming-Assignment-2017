8dd4c6c22fef21d0aeb4502305f7fb69a10f47ed
tracer-ics2017 <tracer@njuics.org>
2017-10-07 21:41:16 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:41:16 up  9:25,  1 user,  load average: 0.06, 0.17, 0.14 ce5a6688896af2e55decde8d51e24cf79cf8f52

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -45,7 +45,7 @@ uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
-  id_dest->val = pio_read(id_src->val, id_dest->width);    
+  id_dest->val = pio_read(id_src->val, id_src->width);    
   operand_write(id_dest, &id_dest->val);
   print_asm_template2(in);
 
@@ -55,7 +55,7 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  pio_write(id_dest->val, id_dest->width, id_src->val);
+  pio_write(id_dest->val, id_src->width, id_src->val);
 
   print_asm_template2(out);
 
