79e122c63ebeb49b1dbb14bad097bea145609153
tracer-ics2017 <tracer@njuics.org>
2017-10-07 13:47:17 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  13:47:17 up  4:16,  1 user,  load average: 0.01, 0.09, 0.09 546d69887d8530257d8f70581f15687779d4c70

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -149,7 +149,9 @@ void difftest_step(uint32_t eip) {
 
   // TODO: Check the registers state with QEMU.
   // Set `diff` as `true` if they are not the same.
-  TODO();
+  
+  if (r.eax != cpu.eax || r.edx != cpu.edx || r.ecx != cpu.ecx || r.ebx != cpu.ebx || r.esp != cpu.esp || r.ebp != cpu.ebp || r.esi != cpu.esi || r.edi != cpu.edi || r.eip != cpu.eip)
+	  diff = true;
 
   if (diff) {
     nemu_state = NEMU_END;
