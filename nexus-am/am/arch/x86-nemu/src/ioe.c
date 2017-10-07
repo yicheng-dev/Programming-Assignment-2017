959e349c5cddaa35435c2d51ae28cb6cd7cd88fb
tracer-ics2017 <tracer@njuics.org>
2017-10-08 00:00:31 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  00:00:31 up  1:39,  1 user,  load average: 0.37, 0.32, 0.21 25fe2d89c91fed226def48bad9d33436a2675a5

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -29,9 +29,9 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   }
   */
   int i,j;
-  for (i = x; i < x + w; i++){
-	for (j = y; j < y + h; j++){
-		fb[i*_screen.width+j] = pixels[(i-x)*w+(j-y)];
+  for (j = y; i < y + h; j++){
+	for (i = x; i < x + w; i++){
+		fb[j*_screen.width+i] = pixels[(j-y)*w+(i-x)];
 	}
   }
 }
