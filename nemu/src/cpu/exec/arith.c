8d509236308fc99b8abe9859d63d66471b759414
tracer-ics2017 <tracer@njuics.org>
2017-10-07 12:38:27 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:38:27 up  3:07,  1 user,  load average: 0.00, 0.03, 0.03 603f92f770f643858bfd44dccffeb1c9c47b96f

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -51,7 +51,8 @@ make_EHelper(cmp) {
 //  rtl_sext(&t2, &id_dest->val, id_dest->width);
 //  rtl_ext(&t1, &id_src->val, id_dest->width);
 //  rtl_ext(&t2, &id_dest->val, id_dest->width);
-  rtl_sext(&id_src->val, &id_src->val, id_dest->width);	
+  if (id_src->width == 1 && (id_dest->width == 2 || id_dest->width == 4))
+	rtl_sext(&id_src->val, &id_src->val, id_dest->width);	
   rtl_sub(&t0, &id_dest->val, &id_src->val);
   rtl_update_ZFSF(&t0, id_dest->width);
 
