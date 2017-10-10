25f3d8f2fa2b10fee63f04709fe04c6656a27c72
tracer-ics2017 <tracer@njuics.org>
2017-10-10 21:15:42 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:15:42 up  4:07,  1 user,  load average: 0.07, 0.02, 0.02 80d09e8661b7a604a8a07ed510bcf87330eea578

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -10,19 +10,21 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&cpu.CS);
   rtl_push(&cpu.eip);
   uint32_t data[2];
-  GateDesc gatedesc;
-//  data[0] = vaddr_read(cpu.idtr.val + NO*8, 4);
-//  data[1] = vaddr_read(cpu.idtr.val + NO*8 + 4, 4);
-  data[0]=0x1234;
-  data[1]=0x5678;
+//  GateDesc gatedesc;
+  rtl_lm(&data[0],&cpu.idtr.val + NO*8, 4);
+  rtl_lm(&data[1],&cpu.idtr.val + NO*8 + 4, 4);
+//  data[0]=0x1234;
+//  data[1]=0x5678;
   printf("data[0]:%x\n",data[0]);
   printf("data[1]:%x\n",data[1]);
   printf("cpu.idtr.val: 0x%x\n",cpu.idtr.val);
-  
-  memcpy(&gatedesc,data,4);
+ /* 
+  memcpy(&gatedesc,&data[0],4);
+  memcpy(&gatedesc+4,&data[1],4);
   decoding.jmp_eip = gatedesc.val;
   decoding.is_jmp = 1;
-  printf("dest: %u\n",gatedesc.val);  
+  printf("dest: %u\n",gatedesc.val);
+  */
 }
 
 void dev_raise_intr() {
