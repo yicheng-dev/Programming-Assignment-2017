ff4498ace8ba36ededd8ef18edbe54d5b0fb5910
tracer-ics2017 <tracer@njuics.org>
2017-10-10 21:18:45 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:18:45 up  4:10,  1 user,  load average: 0.40, 0.18, 0.08 72d07e251b746ae583eeb84a73e2447c7a33348

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -9,15 +9,14 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   rtl_push(&cpu.eflags);
   rtl_push(&cpu.CS);
   rtl_push(&cpu.eip);
-  uint32_t data[2];
-//  GateDesc gatedesc;
-  rtl_lm(&data[0],&cpu.idtr.val + NO*8, 4);
-  rtl_lm(&data[1],&cpu.idtr.val + NO*8 + 4, 4);
+  GateDesc gatedesc;
+  rtl_lm(&gatedesc.val,&cpu.idtr.val + NO*8, 4);
+  rtl_lm(&gatedesc.val+4,&cpu.idtr.val + NO*8 + 4, 4);
 //  data[0]=0x1234;
 //  data[1]=0x5678;
-  printf("data[0]:%x\n",data[0]);
-  printf("data[1]:%x\n",data[1]);
-  printf("cpu.idtr.val: 0x%x\n",cpu.idtr.val);
+//  printf("gatedesc:%x\n",data[0]);
+//  printf("data[1]:%x\n",data[1]);
+//  printf("cpu.idtr.val: 0x%x\n",cpu.idtr.val);
  /* 
   memcpy(&gatedesc,&data[0],4);
   memcpy(&gatedesc+4,&data[1],4);
