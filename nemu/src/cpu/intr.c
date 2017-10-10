8d8d8a6081e391d4a81dd5338d8cf884ec0fcf1f
tracer-ics2017 <tracer@njuics.org>
2017-10-10 18:40:13 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  18:40:13 up  1:49,  1 user,  load average: 0.29, 0.11, 0.24 2eaa862f1f93ff73b1d5b81386598ad547edbcec

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -12,15 +12,12 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
 
   uint32_t data[2], dest;
   printf("cpu.idtr.val: 0x%x\n",cpu.idtr.val);
-  data[0] = vaddr_read(cpu.idtr.val + NO*4, 4);
-  data[1] = vaddr_read(cpu.idtr.val + NO*4 + 4, 4);
+  data[0] = vaddr_read(cpu.idtr.val + NO*8, 8);
+  data[1] = vaddr_read(cpu.idtr.val + NO*8 + 4, 8);
   memcpy(&dest, data, 4);
   decoding.is_jmp = 1;
   decoding.jmp_eip = dest;
-  printf("dest: %p\n",&dest);
-
-
-  
+  printf("dest: %p\n",&dest);  
 }
 
 void dev_raise_intr() {
