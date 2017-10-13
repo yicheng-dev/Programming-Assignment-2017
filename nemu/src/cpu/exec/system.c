c08b718244175a1f01898c108a54af0328a0ffa2
tracer-ics2017 <tracer@njuics.org>
2017-10-13 22:42:46 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  22:42:46 up  7:20,  1 user,  load average: 0.34, 0.47, 0.23 3b1aead2cfe836e9aa860eafe9cc12bbc830f8bf

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -7,7 +7,7 @@ extern void raise_intr(uint8_t NO, vaddr_t ret_addr);
 
 make_EHelper(lidt) {
   cpu.idtr.limit = vaddr_read(id_dest->val,id_dest->width);
-  cpu.idtr.base = vaddr_read(id_dest->addr+2,id_dest->width);
+  cpu.idtr.base = vaddr_read(id_dest->val+2,id_dest->width);
   print_asm_template1(lidt);
 }
 
