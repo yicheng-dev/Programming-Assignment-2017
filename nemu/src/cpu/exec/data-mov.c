c00b132013e34231c392b5e858c940d30a029554
tracer-ics2017 <tracer@njuics.org>
2017-10-13 14:52:09 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:52:09 up  5:04,  1 user,  load average: 0.21, 0.41, 0.46 ee9ea67f325d756e8c11aefcf30c4cb9be6d0a

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -152,12 +152,12 @@ make_EHelper(cltd) {
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
     rtl_lr_b(&t0, R_AL);
-	rtl_sext(&t0,&t0,2);
+	rtl_sext(&t0,&t0,1);
 	rtl_sr_w(R_AX,&t0);
   }
   else {
 	rtl_lr_w(&t0, R_AX);
-	rtl_sext(&t0,&t0,4);
+	rtl_sext(&t0,&t0,2);
 	rtl_sr_l(R_EAX,&t0);
   }
 
