5fc087623fe1c1ac89faed78d0c3249b18411953
tracer-ics2017 <tracer@njuics.org>
2017-10-14 12:06:22 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:06:22 up 11:12,  1 user,  load average: 0.02, 0.21, 0.21 823af0a2f82cacdc772252e01ac21437c4437511

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -12,10 +12,10 @@ struct _RegSet {
   uint32_t error_code, eip, cs, eflags;
 };
 
-#define SYSCALL_ARG1(r) r->eax
-#define SYSCALL_ARG2(r) r->ecx
-#define SYSCALL_ARG3(r) r->edx
-#define SYSCALL_ARG4(r) r->ebx
+#define SYSCALL_ARG1(r) (r->eax)
+#define SYSCALL_ARG2(r) (r->ecx)
+#define SYSCALL_ARG3(r) (r->edx)
+#define SYSCALL_ARG4(r) (r->ebx)
 
 #ifdef __cplusplus
 extern "C" {
