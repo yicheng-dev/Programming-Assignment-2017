fc34aa1955c400444f9c3729e70798fe7f405c55
tracer-ics2017 <tracer@njuics.org>
2017-10-14 11:32:54 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  11:32:54 up 10:39,  1 user,  load average: 0.03, 0.10, 0.08 df86824fce4386a6ab7c25fa3612bc0b30884c48

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,6 +7,8 @@ _RegSet* do_syscall(_RegSet *r) {
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
+	
+  printf("eax:%u;ecx:%u;edx:%u;ebx:%u;esi:%u;edi:%u;esp:%u;ebp:%u\n",r->eax,r->ecx,r->edx,r->ebx,r->esi,r->edi,r->esp,r->ebp);
 
   switch (a[0]) {
 	case SYS_none: a[0]=1; break;
