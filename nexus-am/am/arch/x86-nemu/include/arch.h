887fcd240cec783b0b9ad3114a0893d975bfea10
tracer-ics2017 <tracer@njuics.org>
2017-10-14 11:48:21 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  11:48:21 up 10:54,  1 user,  load average: 0.35, 0.11, 0.08 b5e6f8cf2439bff85b990c12178ef6f09bcc7997

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -13,9 +13,9 @@ struct _RegSet {
 };
 
 #define SYSCALL_ARG1(r) r->eax
-#define SYSCALL_ARG2(r) 0
-#define SYSCALL_ARG3(r) 0
-#define SYSCALL_ARG4(r) 0
+#define SYSCALL_ARG2(r) r->ecx
+#define SYSCALL_ARG3(r) r->edx
+#define SYSCALL_ARG4(r) r->ebx
 
 #ifdef __cplusplus
 extern "C" {
