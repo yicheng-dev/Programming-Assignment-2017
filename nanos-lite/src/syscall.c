5fd9c361c3bc7b61e0d09b798be4fb49ee532c28
tracer-ics2017 <tracer@njuics.org>
2017-10-14 12:15:16 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:15:16 up 11:21,  1 user,  load average: 0.61, 0.32, 0.24 ad03b4fbb171e2775b45ef90c7a22422c6e5f200

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,7 +8,7 @@ _RegSet* do_syscall(_RegSet *r) {
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
 	
-  printf("eax:%u;ecx:%u;edx:%u;ebx:%u;esi:%u;edi:%u;esp:%u;ebp:%u\n",r->eax,r->ecx,r->edx,r->ebx,r->esi,r->edi,r->esp,r->ebp);
+  printf("eax:0x%x;ecx:0x%x;edx:0x%x;ebx:0x%x;esi:0x%x;edi:0x%x;esp:0x%x;ebp:0x%x\n",r->eax,r->ecx,r->edx,r->ebx,r->esi,r->edi,r->esp,r->ebp);
 
   switch (a[0]) {
 	case SYS_none: SYSCALL_ARG1(r)=1; break;
