fa1ab8dea6763f98a2fe709925ad72fed9bfac32
tracer-ics2017 <tracer@njuics.org>
2017-10-14 12:03:20 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:03:20 up 11:09,  1 user,  load average: 0.12, 0.36, 0.25 d7836bef3a9964ceb39dff03bfcf6fae96f3502f

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -28,7 +28,7 @@ make_EHelper(mov_cr2r) {
 }
 
 make_EHelper(int_) {
-  raise_intr(id_dest->val, decoding.seq_eip);/*
+  raise_intr(id_dest->val, *eip);/*
   printf("eax:%u;ecx:%u;edx:%u;ebx:%u;esi:%u;edi:%u;esp:%u;ebp:%u\n",cpu.eax,cpu.ecx,cpu.edx,cpu.ebx,cpu.esi,cpu.edi,cpu.esp,cpu.ebp);*/
   print_asm("int %s", id_dest->str);
 
