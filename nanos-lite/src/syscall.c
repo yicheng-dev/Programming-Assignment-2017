45a2d3c17fe94655765aeba82d3b749c94f74b7f
tracer-ics2017 <tracer@njuics.org>
2017-10-15 16:52:54 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:52:54 up 20:26,  1 user,  load average: 0.29, 0.21, 0.13 a1f34312291769303792780b67a443555042b06

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,9 +6,9 @@ _RegSet* do_syscall(_RegSet *r) {
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
-  	
+/*  	
   printf("eax:0x%x;ecx:0x%x;edx:0x%x;ebx:0x%x;esi:0x%x;edi:0x%x;esp:0x%x;ebp:0x%x\n",r->eax,r->ecx,r->edx,r->ebx,r->esi,r->edi,r->esp,r->ebp);
-
+*/
   switch (a[0]) {
 	case SYS_none: SYSCALL_ARG1(r)=1; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
