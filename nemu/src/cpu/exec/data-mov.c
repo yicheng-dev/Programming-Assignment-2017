90be542c3fcb27de42791d93de84f9212882e850
tracer-ics2017 <tracer@njuics.org>
2017-10-15 14:13:49 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:13:49 up 17:47,  1 user,  load average: 0.18, 0.32, 0.28 62d4d404e397d8ead9b05dcdd14f6b65d708323

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -123,28 +123,11 @@ make_EHelper(leave) {
 
 make_EHelper(cltd) {
   if (decoding.is_operand_size_16) {
-    rtl_lr_w(&t0,R_AX);
-	rtl_li(&t1,0);
-	rtl_slt(&t2,&t0,&t1);
-	if (t2 == 1) {
-		rtl_li(&t0,0xffff);
-		rtl_sr_w(R_DX,&t0);
-	}
-	else{
-		rtl_sr_w(R_DX,&tzero);
-	}
+	rtl_sext(&t0, &reg_l(R_EAX), 2);
+	rtl_shri(&reg_l(R_EDX), &t0, 16);
   }
   else {
-    rtl_lr_l(&t0,R_EAX);
-	rtl_li(&t1,0);
-	rtl_slt(&t2,&t0,&t1);
-	if (t2 == 1){
-		rtl_li(&t0,0xffffffff);
-		rtl_sr_l(R_EDX,&t0);
-	}
-	else{
-		rtl_sr_l(R_EDX,&tzero);
-	}
+	rtl_sari(&reg_l(R_EDX), &reg_l(R_EAX), 31);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
