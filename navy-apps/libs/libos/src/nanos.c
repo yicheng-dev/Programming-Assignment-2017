97da90c47dfbe7568a48b47db2075a513fe18a50
tracer-ics2017 <tracer@njuics.org>
2017-10-22 12:24:16 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:24:16 up  2:04,  1 user,  load average: 0.05, 0.08, 0.08 78964be9725d27a039df999bacf7683ae13adc0c

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -30,8 +30,10 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
-  return _syscall_(SYS_brk, increment, 0, 0);
-//  return (void *)-1;
+  void *old_end = (void *)_heap.end;
+  if ( _syscall_(SYS_brk, increment, 0, 0) == 0);
+    return old_end;
+  return (void *)-1;
 }
 
 int _read(int fd, void *buf, size_t count) {
