fcf61cb4e50f852e0a701806bda7b82c48b6864a
tracer-ics2017 <tracer@njuics.org>
2017-10-23 01:31:24 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  01:31:24 up 11:16,  1 user,  load average: 0.03, 0.06, 0.07 a313cbfb172a4697f9d866cf6dcb38d23d2becaf

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -65,19 +65,20 @@ ssize_t fs_read(int fd, void *buf, size_t len)
 extern void ramdisk_write(const void *,off_t, size_t); 
 ssize_t fs_write(int fd, const void *buf, size_t len)
 {
-  if (len <= file_table[fd].size-file_table[fd].open_offset){
-
+  if (file_table[fd].open_offset >= file_table[fd].size)
+	return 0;
+  else if (len <= file_table[fd].size-file_table[fd].open_offset){
 	printf("write type 1 begin\n"); 
-    ramdisk_write(buf, file_table[fd].disk_offset, len);
-
+    ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset, len);
+	file_table[fd].open_offset += len;
 	printf("write type 1 end\n");
 
 	return len;
   }
-  else{
+  else {
 	printf("write type 2 begin\n");
-	ramdisk_write(buf, file_table[fd].disk_offset, file_table[fd].size- file_table[fd].open_offset);
-	
+	ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset, file_table[fd].size- file_table[fd].open_offset);
+	file_table[fd].open_offset = file_table[fd].size;
 	printf("write type 2 end\n");
 	
 	return file_table[fd].size- file_table[fd].open_offset;
