4555a568c31ceeb0f69b22d6fcb4b3209bfe2a09
tracer-ics2017 <tracer@njuics.org>
2017-10-22 11:17:37 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  11:17:37 up 58 min,  1 user,  load average: 0.13, 0.16, 0.14 640cccfd2acf64cc3a3a458994f1b0abd5729539

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,9 +6,9 @@ _RegSet* do_syscall(_RegSet *r) {
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
-/*  	
+  	
   printf("eax:0x%x;ecx:0x%x;edx:0x%x;ebx:0x%x;esi:0x%x;edi:0x%x;esp:0x%x;ebp:0x%x\n",r->eax,r->ecx,r->edx,r->ebx,r->esi,r->edi,r->esp,r->ebp);
-*/
+
   switch (a[0]) {
 	case SYS_none: r->eax=1; break;
 	case SYS_exit: //printf("eax:0x%x; ecx:0x%x; edx:0x%x; ebx:0x%x\n",r->eax,r->ecx,r->edx,r->ebx);
@@ -16,11 +16,10 @@ _RegSet* do_syscall(_RegSet *r) {
 	case SYS_write: //printf("eax:0x%x; ecx:0x%x; edx:0x%x; ebx:0x%x\n",r->eax,r->ecx,r->edx,r->ebx);break;
 				   if (r->ecx==1 || r->ecx==2){
 					   r->eax=0;
-					   for (;r->eax<r->ebx;r->eax++){
+					   for (; r->eax < r->ebx; r->eax++){
 						   char *tmp=(char*)(r->edx);
 						   _putc(tmp[r->eax]);
 					   }
-
 				   }
 				   break;
     default: panic("Unhandled syscall ID = %d", a[0]);
