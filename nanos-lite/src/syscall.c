4fd63eb5e364035c842a8c8e2635792c26302a50
tracer-ics2017 <tracer@njuics.org>
2017-10-22 12:34:05 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  12:34:05 up  2:14,  1 user,  load average: 0.32, 0.23, 0.14 264dd05a3f922c7b8cad78a1afc023cb15887d5c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -22,7 +22,7 @@ _RegSet* do_syscall(_RegSet *r) {
 					   }
 				   }
 				   break;
-	case SYS_brk:  _heap.end = (void*) SYSCALL_ARG4(r); 
+	case SYS_brk:  _heap.end = (void*)SYSCALL_ARG4(r); 
 				   SYSCALL_ARG1(r)=0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
