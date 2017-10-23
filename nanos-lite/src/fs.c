ec4d9bec60fe607ca2ddfad61653db64c2722f85
tracer-ics2017 <tracer@njuics.org>
2017-10-23 09:24:57 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  09:24:57 up 12:57,  1 user,  load average: 0.26, 0.11, 0.10 a5244020e1e042361eac2ce93662c9b2d4b85ab8

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -101,6 +101,7 @@ off_t fs_lseek(int fd, off_t offset, int whence)
 	case SEEK_END: file_table[fd].open_offset = file_table[fd].size + offset;break;
 	default: return -1;
   }
+
   printf("fs_lseek end, open_offset:%d\n",file_table[fd].open_offset);
   return file_table[fd].open_offset;
 }
