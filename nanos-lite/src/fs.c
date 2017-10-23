e3092d502c272c1690201a5096b30f6e211e3fcc
tracer-ics2017 <tracer@njuics.org>
2017-10-23 16:53:43 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:53:43 up 13:38,  1 user,  load average: 0.16, 0.23, 0.19 19d5a1af3595a707642ca86085386e382105bbf1

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -43,11 +43,11 @@ int fs_open(const char *pathname, int flags, int mode)
 extern void ramdisk_read(void *, off_t, size_t);
 ssize_t fs_read(int fd, void *buf, size_t len)
 {
-  if (file_table[fd].open_offset >= file_table[fd].size - 1){
+  if (file_table[fd].open_offset >= file_table[fd].size){
 	printf("fs_read type 1 begin, fd:%d\nfs_read type 1 end, fd:%d\n",fd,fd);
 	return -1;
   }
-  else if (len <= file_table[fd].size - 1 - file_table[fd].open_offset){
+  else if (len <= file_table[fd].size - file_table[fd].open_offset){
 	printf("fs_read type 2 begin, fd:%d, open_offset:%d, len:%d\n",fd,file_table[fd].open_offset,len);
 	ramdisk_read(buf, file_table[fd].disk_offset+file_table[fd].open_offset, len);
 	file_table[fd].open_offset += len;
@@ -58,23 +58,23 @@ ssize_t fs_read(int fd, void *buf, size_t len)
   }
   else{
 	printf("fs_read type 3 begin, fd:%d, open_offset:%d, len:%d\n",fd,file_table[fd].open_offset,len);
-	ramdisk_read(buf, file_table[fd].disk_offset+file_table[fd].open_offset, file_table[fd].size - 1 - file_table[fd].open_offset);
-	file_table[fd].open_offset = file_table[fd].size - 1;
+	ramdisk_read(buf, file_table[fd].disk_offset+file_table[fd].open_offset, file_table[fd].size - file_table[fd].open_offset);
+	file_table[fd].open_offset = file_table[fd].size;
 
 	printf("fs_read type 3 end, fd:%d, open_offset:%d, len:%d\n",fd,file_table[fd].open_offset,len);
 
-	return file_table[fd].size - file_table[fd].open_offset - 1;
+	return file_table[fd].size - file_table[fd].open_offset;
   }
 }
 
 extern void ramdisk_write(const void *,off_t, size_t); 
 ssize_t fs_write(int fd, const void *buf, size_t len)
 {
-  if (file_table[fd].open_offset >= file_table[fd].size - 1){
+  if (file_table[fd].open_offset >= file_table[fd].size){
 	printf("fs_write type 1 end\n");
 	return -1;
   }
-  else if (len <= file_table[fd].size - 1 - file_table[fd].open_offset){
+  else if (len <= file_table[fd].size - file_table[fd].open_offset){
 	printf("fs_write type 2 begin\n"); 
     ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset, len);
 	file_table[fd].open_offset += len;
@@ -84,11 +84,11 @@ ssize_t fs_write(int fd, const void *buf, size_t len)
   }
   else {
 	printf("fs_write type 3 begin\n");
-	ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset, file_table[fd].size - 1 - file_table[fd].open_offset);
+	ramdisk_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset, file_table[fd].size - file_table[fd].open_offset);
 	file_table[fd].open_offset = file_table[fd].size;
 	printf("fs_write type 3 end\n");
 	
-	return file_table[fd].size - 1 - file_table[fd].open_offset;
+	return file_table[fd].size - file_table[fd].open_offset;
   }
 }
 
