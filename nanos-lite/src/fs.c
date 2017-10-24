1f1cb335a7c138974339591b77398f3af9b193c2
tracer-ics2017 <tracer@njuics.org>
2017-10-24 19:36:54 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:36:54 up 23:51,  1 user,  load average: 0.09, 0.13, 0.09 efe66f226bd95d961207731954930ab69021ccbc

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -84,8 +84,8 @@ ssize_t fs_write(int fd, const void *buf, size_t len)
 				   if (len + file_table[fd].open_offset > file_table[fd].size)
 					   len = file_table[fd].size - file_table[fd].open_offset;
 				   if (len < 0) return -1;
-				   fb_write(buf, file_table[FD_FB].disk_offset+file_table[FD_FB].open_offset, len);
-				   file_table[FD_FB].open_offset += len;
+				   fb_write(buf, file_table[fd].disk_offset+file_table[fd].open_offset, len);
+				   file_table[fd].open_offset += len;
 				   return len;    
 	   default: if (len + file_table[fd].open_offset > file_table[fd].size)
 					len = file_table[fd].size - file_table[fd].open_offset;
