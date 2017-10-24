cb780f5e61167c06aef112c52ad58a81cf8519b1
tracer-ics2017 <tracer@njuics.org>
2017-10-24 23:13:28 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  23:13:28 up 1 day,  2:09,  1 user,  load average: 0.00, 0.04, 0.06 699e27002f665dd91049d46636e828397e416e8c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -8,14 +8,13 @@ static const char *keyname[256] __attribute__((used)) = {
   _KEYS(NAME)
 };
 
+static char event_temp[10000] __attribute__((used));
 size_t events_read(void *buf, size_t len) {
-  int i;
-  while (
-  return 0;
+  memcpy(buf, (void*)event_temp, len);
+  return strlen(buf);
 }
 
 static char dispinfo[128] __attribute__((used));
-static char event_temp[10000] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
 //  printf("fb_read: offset:%d\nlen:%d\n",offset,len);
@@ -37,13 +36,25 @@ extern off_t fs_fileof(int);
 extern void ramdisk_read(void *,off_t, size_t);
 extern ssize_t fs_read(int, void*, size_t);
 extern unsigned long _uptime();
+extern int _read_key();
 void init_device() {
   _ioe_init();
   int key = _read_key();
-  if (key != _KEY_NONE) sprintf(event_temp, "kd %s", name(key));
-  else if 
-  
-  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", _screen.width, _screen.height);
+  bool down =false;
+  if (key & 0x8000) {
+	key = key ^ 0x8000;
+	down =true;
+  }
+  if (key != _KEY_NONE){
+	if (down)
+	  sprintf(event_temp, "kd %s\n", keyname[key]);
+	else
+	  sprintf(event_temp, "ku %s\n", keyname[key]);
+  }
+  else {
+	sprintf(event_temp, "t %d\n", _uptime());
+  } 
+    sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", _screen.width, _screen.height);
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
 }
