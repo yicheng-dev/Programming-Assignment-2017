b917ab444cdcb1b546ecec8fc4964e6c2157f3ac
tracer-ics2017 <tracer@njuics.org>
2017-10-25 00:32:40 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  00:32:40 up 1 day,  3:28,  1 user,  load average: 0.43, 0.30, 0.23 304af6351b7b47ab38563a51557e01e8a2c9df40

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -15,8 +15,8 @@ unsigned long _uptime() {
 uint32_t* const fb = (uint32_t *)0x40000;
 
 _Screen _screen = {
-  .width  = 320,
-  .height = 200,
+  .width  = 400,
+  .height = 300,
 };
 
 extern void* memcpy(void *, const void *, int);
