5b0b673dbbd251f3c13b62873495857d578173ff
tracer-ics2017 <tracer@njuics.org>
2017-10-25 11:17:48 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  11:17:49 up  1:03,  1 user,  load average: 0.21, 0.11, 0.10 bb82da46cbc08d8ab15ef6e6bab0f9e5f82b559

--- navy-apps/apps/pal/src/hal/hal.c
+++ navy-apps/apps/pal/src/hal/hal.c
@@ -249,15 +249,13 @@ SDL_Surface* SDL_CreateRGBSurface(uint32_t flags, int width, int height, int dep
 
   s->format->BitsPerPixel = depth;
   
-  s->flags = 1;
-//  s->flags = flags;
+  s->flags = flags;
   
   printf("s->flags:%d\n",s->flags);
   s->w = width;
   s->h = height;
 
-  s->pitch = 320;
-//  s->pitch = (width * depth) >> 3;
+  s->pitch = (width * depth) >> 3;
   printf("s->pitch:%d\n",s->pitch);
   s->pixels = (flags & SDL_HWSURFACE ? (void *)VMEM_ADDR : malloc(s->pitch * height));
   assert(s->pixels);
