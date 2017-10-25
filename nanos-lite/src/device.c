a1cfa665bf2da03214ee704a870b81d1a0a4beca
tracer-ics2017 <tracer@njuics.org>
2017-10-25 09:36:35 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  09:36:35 up 1 day,  5:27,  1 user,  load average: 0.61, 0.36, 0.29 c4c23b1d60adde6a197184efa9b5344f522a8654

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -40,10 +40,6 @@ static char dispinfo[128] __attribute__((used));
 void dispinfo_read(void *buf, off_t offset, size_t len) {
 //  printf("fb_read: offset:%d\nlen:%d\n",offset,len);
   memcpy(buf, (void*)dispinfo+offset, len);
-  
-  printf("(void*)dispinfo+offset: %s\n",(void*)dispinfo+offset);
-  printf("dispinfo+offset: %s\n",dispinfo+offset);
-  
 
 //  printf("buf:%s\n",buf);
 }
