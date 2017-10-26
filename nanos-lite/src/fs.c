d5dc2fc0079ab74b896e858a956ac2156b5e8594
tracer-ics2017 <tracer@njuics.org>
2017-10-26 21:24:55 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:24:55 up  4:51,  1 user,  load average: 0.28, 0.41, 0.39 8e51cfbb8d984858d0d53c62351f950a78ae3fab

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,11 +58,7 @@ ssize_t fs_read(int fd, void *buf, size_t len)
 			return len;
 	    case FD_EVENTS:
 //			printf("len: %d\n",len);
-			if (len + file_table[fd].open_offset > file_table[fd].size)
-			  len = file_table[fd].size - file_table[fd].open_offset;
-			if (len < 0 ) return -1;
-			int rlen = events_read(buf, len);
-			return rlen;
+			return events_read(buf, len);
 		default:
 			if (len + file_table[fd].open_offset > file_table[fd].size)
 				len = file_table[fd].size - file_table[fd].open_offset;
