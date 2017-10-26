75500c52c685c9a0499f5c360e133f223fa5e3f9
tracer-ics2017 <tracer@njuics.org>
2017-10-26 22:24:52 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  22:24:52 up  5:51,  1 user,  load average: 0.59, 0.40, 0.32 3b90cccf894e40e957f076933ed138b8ef3e16c1

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -8,60 +8,37 @@ static const char *keyname[256] __attribute__((used)) = {
   _KEYS(NAME)
 };
 
-extern off_t fs_fileof(int);
-//static char event_temp[10000] __attribute__((used));
 size_t events_read(void *buf, size_t len) {
-  char events[100];
+  char tem[128];
   int key = _read_key();
-  bool down =false;
-  if (key & 0x8000) {
-	key = key ^ 0x8000;
-    down =true;
+  int l;
+  //Log("get in");
+  if (key!=_KEY_NONE) {
+	if (key >= 0x8000)
+		sprintf(tem,"kd %s\n",keyname[key - 0x8000]);
+    else sprintf(tem,"ku %s\n",keyname[key]);
+	l = strlen(tem) < len ? strlen(tem) : len;
+	memcpy(buf,tem,l);
+	return l;
   }
-  
-  if (key != _KEY_NONE){
-//	printf("key:%d\n",key);
-	if (down)
-	  sprintf(events, "kd %s\n", keyname[key]);
-	else
-	  sprintf(events, "ku %s\n", keyname[key]);
-  }
-  else {
-    sprintf(events, "t %d\n", _uptime());
-  }
-  if (len > strlen(buf)) len = strlen(buf);
-  memcpy(buf, (void*)events, len);
-//  printf("buf:%s\n",buf);
-//  printf("strlen(buf):%d\n",strlen(buf));
-  return len;
+  sprintf(tem,"t %d\n",_uptime());
+  l = strlen(tem) < len ? strlen(tem) : len;
+  memcpy(buf,tem,l);
+  return l;
 }
 
 static char dispinfo[128] __attribute__((used));
-
 void dispinfo_read(void *buf, off_t offset, size_t len) {
-//  printf("fb_read: offset:%d\nlen:%d\n",offset,len);
-  memcpy(buf, (void*)dispinfo+offset, len);
-
-//  printf("buf:%s\n",buf);
+	memcpy(buf,dispinfo+offset,len);
 }
-
 extern uint32_t* const fb;
 void fb_write(const void *buf, off_t offset, size_t len) {
-//  printf("fb_write: offset:%d\nlen:%d\n",offset,len);
-  memcpy((void*)fb+offset, buf, len);
+	memcpy(fb+offset/4,buf,len);
 }
 
-extern int fs_open(const char *, int, int);
-extern ssize_t fs_write(int, const void*, size_t);
-extern int fs_close(int);
-extern size_t fs_filesz(int);
-extern void ramdisk_read(void *,off_t, size_t);
-extern ssize_t fs_read(int, void*, size_t);
-extern unsigned long _uptime();
-extern int _read_key();
 void init_device() {
-  _ioe_init(); 
-  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", _screen.width, _screen.height);
+  _ioe_init();
+  sprintf(dispinfo,"WIDTH:%d\nHEIGHT:%d",_screen.width,_screen.height);
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
 }
