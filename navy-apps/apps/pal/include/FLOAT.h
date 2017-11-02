4c8b92dc72712056f6149e049d03fff5b08ee40a
tracer-ics2017 <tracer@njuics.org>
2017-11-02 08:15:49 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  08:15:49 up 19:26,  1 user,  load average: 0.77, 0.56, 0.80 69dd0a04204b05eb23d3f2defa8e524fa01ab451

--- navy-apps/apps/pal/include/FLOAT.h
+++ navy-apps/apps/pal/include/FLOAT.h
@@ -2,27 +2,24 @@
 #define __FLOAT_H__
 
 #include "assert.h"
+#include "math.h"
 
 typedef int FLOAT;
 
 static inline int F2int(FLOAT a) {
-  assert(0);
-  return 0;
+  return a/pow(2,16);
 }
 
 static inline FLOAT int2F(int a) {
-  assert(0);
-  return 0;
+  return a * pow(2,16);
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
-  assert(0);
-  return 0;
+  return a * int2F(b);
 }
 
 static inline FLOAT F_div_int(FLOAT a, int b) {
-  assert(0);
-  return 0;
+  return a/int2F(b);
 }
 
 FLOAT f2F(float);
