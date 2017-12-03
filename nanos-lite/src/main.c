5043f334a6135a68375843857ef151fc452c4759
tracer-ics2017 <tracer@njuics.org>
2017-12-03 16:26:30 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:26:30 up  5:51,  1 user,  load average: 0.54, 0.22, 0.07 b26b7a8aa7c3820e421edea2bdec184682fb88ef

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,7 +33,7 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL, "/bin/dummy");
+  uint32_t entry = loader(NULL, "/bin/pal");
   ((void (*)(void))entry)();
 
 //  load_prog("/bin/dummy");
