c0f77e643eb8a46978f18ac9a33840f2d0bdacc7
tracer-ics2017 <tracer@njuics.org>
2017-12-05 14:33:59 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  14:33:59 up 16:45,  1 user,  load average: 0.34, 0.20, 0.14 c7eca7c949e08cf28892c38664e8b09129cdef54

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -16,13 +16,13 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uint32_t new_brk) {
+	  printf("new_brk: 0x%x\tcurrent->max_brk: 0x%x\n",new_brk, current->max_brk);
   if (current->cur_brk == 0) {
 	current->cur_brk = current->max_brk = new_brk;
   }
   else {
 	if (new_brk > current->max_brk) {
 
-	  printf("new_brk: 0x%x\tcurrent->max_brk: 0x%x\n",new_brk, current->max_brk);
 	  // TODO: map memory region [current->max_brk, new_brk)
 	  // into address space current->as
 	  _map(&current->as, (void*)current->max_brk, (void*)new_brk);
