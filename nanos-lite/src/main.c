731144e57c8320674f90674745b4cc857e945391
tracer-ics2017 <tracer@njuics.org>
2017-12-10 00:28:54 +0800

    >  compile 161220049 broccoli Linux ubuntu 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  00:28:54 up 20:43,  1 user,  load average: 0.84, 0.32, 0.38 cfc31d6e4632f915f95ba12a4e3e1defd3e48ca4

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -36,6 +36,6 @@ int main() {
   uint32_t entry = loader(NULL, "/bin/pal");
   ((void (*)(void))entry)();
 */
-  load_prog("/bin/pal");
+  load_prog("/bin/dummy");
   panic("Should not reach here");
 }
