d168bd2f5782ba7cc164027df52935939202af50
tracer-ics2017 <tracer@njuics.org>
2017-12-12 21:16:09 +0800

    >  run 161220049 broccoli Linux ubuntu 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  21:16:09 up 1 day, 32 min,  1 user,  load average: 0.24, 0.39, 0.36 f8794b20cc24423e7444661b53b427879dc6fb3a

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -35,9 +35,11 @@ _RegSet* schedule(_RegSet *prev) {
 	current = &pcb[0];
 	first_process = false;
   }
-  if (process_cnt == 1000) process_cnt=0;
-  else if (process_cnt % 2 == 0){
+  else if (current == &pcb[1])
 	current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  else if (process_cnt>=1000 && current == &pcb[0]){
+	current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+	process_cnt = 0;
   }
   process_cnt++;
   _switch(&current->as);
